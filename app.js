2a909f2e2c947f41e5e03bd944f3987c36118cc0
Nick Simmons <nick.simmons.514@gmail.com>
2013-01-24 21:40:46 +0800

    - Moved static folder to client - Preload main.html in server and return it as server root

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 "use strict";
+var fs = require('fs');
 
 // Import modules
 var express = require('express');
 
+var rootHtml = fs.readFileSync('client/main.html');
+
 // Create HTTP server
 var app = express();
 
-// Allows static files to be returned
-app.use(express.static(__dirname + '/static'));
+// Allows client files to be served by node
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(req, res) {
+    res.set('Content-Type', 'text/html');
+    res.send(rootHtml);
+});
 
 app.listen(8080);
