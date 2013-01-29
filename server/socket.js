b6cd48251a0978a6c0571c6012021455c7edef31
Nick Simmons <nick.simmons.514@gmail.com>
2013-01-30 00:04:44 +0800

    - Fixed match connect logic

--- server/socket.js
+++ server/socket.js
@@ -10,12 +10,12 @@ exports = module.exports = function(app, db, logger) {
     var io = socketIO.listen(app, { logger: logger });
     // Create event handlers
     io.sockets.on('connection', function (socket) {
-        configureSocket(socket, db, logger);
+        configureSocket(socket, io, db, logger);
         socket.emit('getMatch');
     });
 };
 
-function configureSocket(socket, db, logger) {
+function configureSocket(socket, io, db, logger) {
     socket.on('terminate', function () {
         // Close connection
         socket.disconnect();
@@ -23,12 +23,18 @@ function configureSocket(socket, db, logger) {
     socket.on('setMatch', function (matchKey) {
         // Add match info to socket
         socket.set('matchKey', matchKey, function () {});
+        // Join match channel
+        socket.join(matchKey);
+        // Update match
         MatchManager.connectToMatch(db, matchKey, function(err, startMatch) {
             if (err) {
                 logger.logException(err);
                 socket.emit('error', err.message)
             }
-            socket.emit('startGame');
+            // If all players are connected start the match
+            if (startMatch) {
+                io.sockets.in(matchKey).emit('startGame');
+            }
         });
     });
 }
